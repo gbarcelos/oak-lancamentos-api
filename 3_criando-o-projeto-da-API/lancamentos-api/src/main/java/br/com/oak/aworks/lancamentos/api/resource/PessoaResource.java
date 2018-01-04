b3219f7221a9bb6cec7b257d13d5e59f25e9c48c
Gustavo Barcelos <gustavo.barcelos@bancobmg.com.br>
2018-01-04 08:54:25 -0200

    Atualizando pessoa com PUT

--- 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/resource/PessoaResource.java
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/resource/PessoaResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.oak.aworks.lancamentos.api.event.RecursoCriadoEvent;
 import br.com.oak.aworks.lancamentos.api.model.Pessoa;
 import br.com.oak.aworks.lancamentos.api.repository.PessoaRepository;
+import br.com.oak.aworks.lancamentos.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -28,6 +30,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+
+	@Autowired
+	private PessoaService pessoaService;
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -61,5 +66,13 @@ public class PessoaResource {
 
 		pessoaRepository.delete(codigo);
 
+	}
+
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+
+		return ResponseEntity.ok(pessoaSalva);
 	}
 }
---
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/service/PessoaService.java
@@ -0,0 +1,29 @@
+package br.com.oak.aworks.lancamentos.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.oak.aworks.lancamentos.api.model.Pessoa;
+import br.com.oak.aworks.lancamentos.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+
+		return pessoaRepository.save(pessoaSalva);
+	}
+}
