f4b61e8e3a06b4161bedbfdbac3fc92a7e2a6881
Gustavo Barcelos <gustavo.barcelos@bancobmg.com.br>
2018-01-22 08:46:26 -0200

    Profiles do Spring

---
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/config/property/LancamentosApiProperty.java
@@ -0,0 +1,38 @@
+package br.com.oak.aworks.lancamentos.api.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("lancamentosapiproperty")
+public class LancamentosApiProperty {
+
+	// http://www.baeldung.com/configuration-properties-in-spring-boot
+
+	private String originPermitida = "http://localhost:8000";
+
+	private final Seguranca seguranca = new Seguranca();
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}
+
+	public String getOriginPermitida() {
+		return originPermitida;
+	}
+
+	public void setOriginPermitida(String originPermitida) {
+		this.originPermitida = originPermitida;
+	}
+
+	public static class Seguranca {
+
+		private boolean enableHttps;
+
+		public boolean isEnableHttps() {
+			return enableHttps;
+		}
+
+		public void setEnableHttps(boolean enableHttps) {
+			this.enableHttps = enableHttps;
+		}
+	}
+}
