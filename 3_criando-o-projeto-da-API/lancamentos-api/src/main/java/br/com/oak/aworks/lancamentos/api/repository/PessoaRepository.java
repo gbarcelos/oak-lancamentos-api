ac25a9ab995a5a1b26642919a474bdaa5f1fa07d
Gustavo Barcelos <gustavo.barcelos@bancobmg.com.br>
2018-02-05 09:17:20 -0200

    Pesquisa de pessoa

--- 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/repository/PessoaRepository.java
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/repository/PessoaRepository.java
@@ -3,7 +3,8 @@ package br.com.oak.aworks.lancamentos.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.oak.aworks.lancamentos.api.model.Pessoa;
+import br.com.oak.aworks.lancamentos.api.repository.pessoa.PessoaRepositoryQuery;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>, PessoaRepositoryQuery {
 
 }
---
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/repository/pessoa/PessoaRepositoryQuery.java
@@ -0,0 +1,13 @@
+package br.com.oak.aworks.lancamentos.api.repository.pessoa;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import br.com.oak.aworks.lancamentos.api.model.Pessoa;
+import br.com.oak.aworks.lancamentos.api.repository.filter.PessoaFilter;
+
+public interface PessoaRepositoryQuery {
+	
+	public Page<Pessoa> filtrar(PessoaFilter pessoaFilter, Pageable pageable);
+
+}
