07d46baae1826320bf380740f419914491b74f5b
Gustavo Barcelos <gustavo.barcelos@bmgmoney.com>
2018-06-26 08:15:27 -0300

    Log e novo encriptador de senha

--- 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/config/BasicSecurityConfig.java
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/config/BasicSecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.oak.aworks.lancamentos.api.config;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -9,24 +11,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.oak.aworks.lancamentos.api.config.property.LancamentosApiProperty;
+import br.com.oak.aworks.lancamentos.api.security.AppUserDetailsService;
+import br.com.oak.aworks.lancamentos.api.security.LancamentoPasswordEncoder;
 
 @Profile("basic-security")
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final Logger LOGGER = Logger.getLogger(BasicSecurityConfig.class);
 
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private LancamentosApiProperty lancamentosApiProperty;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String originPermitida = lancamentosApiProperty.getOriginPermitida();
+		
+		LOGGER.log(Level.INFO, " -> configure(AuthenticationManagerBuilder auth)");
+		
+		LOGGER.log(Level.INFO, " -> originPermitida: " + originPermitida);
+		
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new LancamentoPasswordEncoder();
 	}
 
 	@Override
