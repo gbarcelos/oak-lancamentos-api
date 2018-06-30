96b8447f5ff6a0939c51a9543f5402e154510ad7
Gustavo Barcelos <gustavo.barcelos@bmgmoney.com>
2018-06-30 10:08:36 -0300

    Criando o DTO do relatório

--- 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/model/TipoLancamento.java
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/model/TipoLancamento.java
@@ -1,6 +1,18 @@
 package br.com.oak.aworks.lancamentos.api.model;
 
 public enum TipoLancamento {
-	RECEITA,
-	DESPESA
+	
+	RECEITA("Receita"),
+	DESPESA("Despesa")
+	;
+	
+	private final String descricao;
+	
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 }
