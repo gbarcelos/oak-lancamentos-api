914854302ed3f3b27e219d0c311085ec5ca20b2b
Gustavo Barcelos <gustavo.barcelos@bmgmoney.com>
2018-07-03 22:48:01 -0300

    Resolvendo o StackOverflowError com @JsonIgnoreProperties

--- 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/model/Pessoa.java
+++ 3_criando-o-projeto-da-API/lancamentos-api/src/main/java/br/com/oak/aworks/lancamentos/api/model/Pessoa.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "pessoa")
@@ -35,6 +36,7 @@ public class Pessoa {
 	@NotNull
 	private Boolean ativo;
 	
+	@JsonIgnoreProperties("pessoa")
 	@Valid
 	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
 	private List<Contato> contatos;
@@ -75,6 +77,14 @@ public class Pessoa {
 	@Transient
 	public boolean isInativo() {
 		return !this.ativo;
+	}
+
+	public List<Contato> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(List<Contato> contatos) {
+		this.contatos = contatos;
 	}
 
 	@Override
